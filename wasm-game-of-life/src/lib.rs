c30bd6bf054ac5efa5727596a5e3260b8673b41e
Manish Kumar <manis.kumar@gmail.com>
2021-11-22 20:48:39 +0530

    Added test cases.

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -12,22 +12,26 @@ use fixedbitset::FixedBitSet;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-#[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Cell {
-  Dead = 0,
-  Alive = 1,
-}
-
-#[wasm_bindgen]
 pub struct Universe {
   width: u32,
   height: u32,
   cells: FixedBitSet,
 }
 
-
 impl Universe {
+  pub fn get_cells(&self) -> &FixedBitSet {
+    &self.cells
+  }
+
+  /// Set cells to be alive in a universe by passing the row and column
+  /// of each cell as an array.
+  pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+    for (row, col) in cells.iter().cloned() {
+      let idx = self.get_index(row, col);
+      self.cells.set(idx, true)
+    }
+  }
+
   fn get_index(&self, row: u32, column: u32) -> usize {
     (row * self.width + column) as usize
   }
@@ -68,6 +72,24 @@ impl Universe {
       height,
       cells,
     }
+  }
+
+  /// Set the width of the universe.
+  ///
+  /// Resets all cells to the dead state.
+  pub fn set_width(&mut self, width: u32) {
+    self.width = width;
+    let size = (self.width * self.height) as usize;
+    self.cells = FixedBitSet::with_capacity(size);
+  }
+
+  /// Set the height of the universe.
+  ///
+  /// Resets all cells to the dead state.
+  pub fn set_height(&mut self, height: u32) {
+    self.height = height;
+    let size = (self.width * self.height) as usize;
+    self.cells = FixedBitSet::with_capacity(size);
   }
 
   pub fn width(&self) -> u32 {
